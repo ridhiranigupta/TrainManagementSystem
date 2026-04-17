9836cce8bdd73ac262f1b3ce959f2c7f2cd280f6
ridhiranigupta <ridhigupta98452@gmail.com>
2026-04-17 16:35:01 +0530

    UC19

--- App/src/TrainManagementTest.java
+++ App/src/TrainManagementTest.java
@@ -6,32 +6,30 @@ public class TrainManagementTest {
     @Test
     void testSearch_BogieFound() {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        boolean result = TrainManagementSystem.linearSearch(bogieIds, "BG309");
-        assertTrue(result);
+        assertTrue(TrainManagementSystem.binarySearch(bogieIds, "BG309"));
     }
 
     @Test
     void testSearch_BogieNotFound() {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        boolean result = TrainManagementSystem.linearSearch(bogieIds, "BG999");
-        assertFalse(result);
+        assertFalse(TrainManagementSystem.binarySearch(bogieIds, "BG999"));
     }
 
     @Test
     void testSearch_FirstElementMatch() {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        assertTrue(TrainManagementSystem.linearSearch(bogieIds, "BG101"));
+        assertTrue(TrainManagementSystem.binarySearch(bogieIds, "BG101"));
     }
 
     @Test
     void testSearch_LastElementMatch() {
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
-        assertTrue(TrainManagementSystem.linearSearch(bogieIds, "BG550"));
+        assertTrue(TrainManagementSystem.binarySearch(bogieIds, "BG550"));
     }
 
     @Test
     void testSearch_SingleElementArray() {
         String[] bogieIds = {"BG101"};
-        assertTrue(TrainManagementSystem.linearSearch(bogieIds, "BG101"));
+        assertTrue(TrainManagementSystem.binarySearch(bogieIds, "BG101"));
     }
 }
